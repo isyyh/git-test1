182c8b179477e44741af80c11706a178f20ae1c5
isyyh <isyyh@qq.com>
2023-02-15 18:33:12 +0800

    1

---
+++ src/main/java/org/example/test1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @author isyyh
+ * @version 1.0
+ * @description TODO
+ * @date 2023/2/15 18:31
+ */
+public class test1 {
+
+	public static void main(String[] args) {
+		System.out.println("23");
+	}
+
+}
